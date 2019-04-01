60071a6aaba856bd5c1e76c24ab435ba6a8738e4
Liubou Sakalouskaya <ly2708sokol@gmail.com>
2019-04-01 10:22:55 +0300

    Ex. 4

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -1,5 +1,30 @@
 #import "MatrixHacker.h"
-// your code here
+
+@interface MatrixHacker ()
+@property (nonatomic, copy) id<Character>(^savedBlock)(NSString *);
+@end
+
 @implementation MatrixHacker
-// your code here
+- (void)injectCode:(id<Character> (^)(NSString *))theBlock {
+    //[_savedBlock release];
+    //_savedBlock = [theBlock copy];
+    self.savedBlock = theBlock;
+}
+
+- (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
+    NSMutableArray *people = [NSMutableArray new];
+    for (NSString *name in names) {
+        [people addObject: self.savedBlock(name)];
+    }
+    NSArray<id<Character>> *result = [[people copy] autorelease];
+    [people release];
+    return result;
+}
+
+- (void)dealloc
+{
+    [_savedBlock release];
+    _savedBlock = nil;
+    [super dealloc];
+}
 @end
--- Tasks/Task2/RSSchool_T2/RSSchool_T2Tests/MatrixHackerTests.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2Tests/MatrixHackerTests.m
@@ -7,8 +7,21 @@
 @end
 
 @interface Char : NSObject <Character>
+@property (nonatomic, getter=isClone) BOOL clone;
+@property (nonatomic, copy) NSString *name;
 @end
 @implementation Char
++ (instancetype)createWithName:(NSString *)name isClone:(BOOL)clone {
+    Char *person = [Char new];
+    person.name = name;
+    person.clone = clone;
+    return [person autorelease];
+}
+- (void)dealloc
+{
+    [_name release];
+    [super dealloc];
+}
 @end
 
 @implementation MatrixHackerTests
@@ -21,8 +34,12 @@
 - (void)test1 {
   __block NSInteger counter = 0;
   [self.hacker injectCode:^id<Character>(NSString *name) {
-    counter += 1;
-    return [Char new];
+      counter += 1;
+      if ([name isEqualToString: @"Neo"]) {
+          return [Char createWithName:@"Neo" isClone:NO];
+      } else {
+          return [Char createWithName:@"Agent Smith" isClone:YES];
+      }
   }];
   [self.hacker runCodeWithData:self.people];
   XCTAssertTrue(self.people.count == counter);
